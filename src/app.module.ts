a5ce54fbe5953c9db2aafb93a132a3e381565b62
hyeg0121 <w2218@e-mirim.hs.kr>
2024-02-05 13:27:48 +0900

    feat: 몽고디비 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,17 @@ import { FocusModule } from './focus/focus.module';
 import { FocusRecordModule } from './focus-record/focus-record.module';
 import { GoalModule } from './goal/goal.module';
 import { TaskModule } from './task/task.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UserModule, FocusModule, FocusRecordModule, GoalModule, TaskModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/fodo'),
+    UserModule,
+    FocusModule,
+    FocusRecordModule,
+    GoalModule,
+    TaskModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
