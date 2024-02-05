e00fba630fc3489265b9516c8c71257f9a35d551
hyeg0121 <w2218@e-mirim.hs.kr>
2024-02-05 13:28:25 +0900

    feat: user module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,17 @@
+// user.module.ts
 import { Module } from '@nestjs/common';
-import { UserController } from './user.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserRepository } from './user.repository';
+import { User, UserSchema } from './user.schema';
 import { UserService } from './user.service';
+import { UserController } from './user.controller';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
+  providers: [UserRepository, UserService],
   controllers: [UserController],
-  providers: [UserService]
+  // exports: [UserService],
 })
 export class UserModule {}
