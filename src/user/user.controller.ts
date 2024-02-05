c52620c60c8a553373a4bc594584a0c0a23609c3
hyeg0121 <w2218@e-mirim.hs.kr>
2024-02-05 13:28:14 +0900

    feat: user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { UserDto } from './user.model';
 
-@Controller('user')
-export class UserController {}
+@Controller('users')
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Get()
+  getAllUsers() {
+    return this.userService.getAllUsers();
+  }
+
+  @Get('/:id')
+  getUser(@Param('id') id: string) {
+    return this.userService.getUserById(id);
+  }
+
+  @Post()
+  createUser(@Body() userDto: UserDto) {
+    return this.userService.createUser(userDto);
+  }
+
+  @Put('/:id')
+  updateUser(@Param('id') id: string, @Body() userDto: UserDto) {
+    return this.userService.updateUser(id, userDto);
+  }
+
+  @Delete('/:id')
+  deleteUser(@Param('id') id: string) {
+    this.userService.deleteUser(id);
+    return 'success';
+  }
+}
