4e909de8674957c18eed13a4e1bc902c25599979
hyeg0121 <w2218@e-mirim.hs.kr>
2024-02-05 11:48:46 +0900

    feat: task module, controller, service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,15 @@
+// @ts-ignore
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { FocusModule } from './focus/focus.module';
-import { FocusRecordModule } from './focus_record/focus_record.module';
 import { FocusRecordModule } from './focus-record/focus-record.module';
 import { GoalModule } from './goal/goal.module';
+import { TaskModule } from './task/task.module';
 
 @Module({
-  imports: [UserModule, FocusModule, FocusRecordModule, GoalModule],
+  imports: [UserModule, FocusModule, FocusRecordModule, GoalModule, TaskModule],
   controllers: [AppController],
   providers: [AppService],
 })
