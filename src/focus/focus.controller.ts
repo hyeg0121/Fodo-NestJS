20a55930e19a93b4c1ebfa45ce917334ce57b9bb
hyeg0121 <w2218@e-mirim.hs.kr>
2024-02-07 17:40:09 +0900

    feat: focus controller

--- src/focus/focus.controller.ts
+++ src/focus/focus.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
+import { FocusService } from "./focus.service";
+import { FocusDto } from "./focus.model";
+
 
 @Controller('focus')
-export class FocusController {}
+export class FocusController {
+  constructor(private focusService: FocusService) {}
+
+  @Get()
+  getAllFocus() {
+    return this.focusService.getAllFocus();
+  }
+
+  @Get('/:id')
+  getFocus(@Param('id') id: string) {
+    return this.focusService.getFocusById(id);
+  }
+
+  @Post()
+  createFocus(@Body() focusDto: FocusDto) {
+    return this.focusService.createFocus(focusDto);
+  }
+
+  @Put('/:id')
+  updateFocus(@Param('id') id: string, @Body() focusDto: FocusDto) {
+    return this.focusService.updateFocus(id, focusDto);
+  }
+
+  @Delete('/:id')
+  deleteFocus(@Param('id') id: string) {
+    this.focusService.deleteFocus(id);
+    return 'success';
+  }
+}
