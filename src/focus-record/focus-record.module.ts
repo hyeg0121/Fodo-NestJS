7ec9e4315c83c29cf684ca04fe5b698f8a832145
hyeg0121 <w2218@e-mirim.hs.kr>
2024-02-07 23:02:03 +0900

    feat: focus record module

--- src/focus-record/focus-record.module.ts
+++ src/focus-record/focus-record.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { FocusRecordController } from './focus-record.controller';
 import { FocusRecordService } from './focus-record.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { FocusRecordRepository } from './focus-record.repository';
+import { FocusRecord, FocusRecordSchema } from "./focus-record-schema";
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: FocusRecord.name, schema: FocusRecordSchema },
+    ]),
+  ],
   controllers: [FocusRecordController],
-  providers: [FocusRecordService]
+  providers: [FocusRecordRepository, FocusRecordService],
 })
 export class FocusRecordModule {}
