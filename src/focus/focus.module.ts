f2a4465b655c914419f9b4520825cd9d60393f6f
hyeg0121 <w2218@e-mirim.hs.kr>
2024-02-07 17:40:03 +0900

    feat: focus module

--- src/focus/focus.module.ts
+++ src/focus/focus.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { FocusController } from './focus.controller';
 import { FocusService } from './focus.service';
+import { MongooseModule } from "@nestjs/mongoose";
+import { Focus, FocusSchema } from "./focus.schema";
+import { FocusRepository } from "./focus.repository";
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Focus.name, schema: FocusSchema }]),
+  ],
   controllers: [FocusController],
-  providers: [FocusService]
+  providers: [FocusRepository, FocusService]
 })
 export class FocusModule {}
