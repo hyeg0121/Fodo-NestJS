d90b81f66b474cfe9fe0ad32af71fd8af51b8663
hyeg0121 <w2218@e-mirim.hs.kr>
2024-02-07 23:02:17 +0900

    feat: focus record controller

--- src/focus-record/focus-record.controller.ts
+++ src/focus-record/focus-record.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
+import { FocusRecordService } from "./focus-record.service";
+import { FocusRecordDto } from "./focus-record.model";
 
-@Controller('focus-record')
-export class FocusRecordController {}
+
+@Controller('focus-records')
+export class FocusRecordController {
+  constructor(private focusRecordService: FocusRecordService) {}
+
+  @Get()
+  getAllFocusRecords() {
+    return this.focusRecordService.getAllFocusRecord();
+  }
+
+  @Get('/:id')
+  getFocusRecord(@Param('id') id: string) {
+    return this.focusRecordService.getFocusRecordById(id);
+  }
+
+  @Post()
+  createFocusRecord(@Body() focusRecordDto: FocusRecordDto) {
+    return this.focusRecordService.createFocusRecord(focusRecordDto);
+  }
+
+  @Put('/:id')
+  updateFocusRecord(@Param('id') id: string, @Body() focusRecordDto: FocusRecordDto) {
+    return this.focusRecordService.updateFocusRecord(id, focusRecordDto);
+  }
+
+  @Delete('/:id')
+  deleteFocusRecord(@Param('id') id: string) {
+    this.focusRecordService.deletedFocusRecord(id);
+    return 'success';
+  }
+}
