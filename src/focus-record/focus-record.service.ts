9dc87c7ecc6fd2e75fe7b21eb5645b19cacea61d
hyeg0121 <w2218@e-mirim.hs.kr>
2024-02-07 23:00:40 +0900

    feat: focus record service

--- src/focus-record/focus-record.service.ts
+++ src/focus-record/focus-record.service.ts
@@ -1,4 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { FocusRecordRepository } from './focus-record.repository';
+import { FocusRecord } from './focus-record-schema';
+import { FocusRecordDto } from './focus-record.model';
 
 @Injectable()
-export class FocusRecordService {}
+export class FocusRecordService {
+  constructor(private focusRecordRepository: FocusRecordRepository) {}
+
+  // 모든 포커스 레코드 조회
+  async getAllFocusRecord(): Promise<FocusRecord[]> {
+    return await this.focusRecordRepository.getAllFocusRecords();
+  }
+
+  // id로 포커스 레코드 조회
+  async getFocusRecordById(id: string): Promise<FocusRecordDto> {
+    return await this.focusRecordRepository.getFocusRecordById(id);
+  }
+
+  // 포커스 레코드 생성
+  async createFocusRecord(
+    focusRecordDto: FocusRecordDto,
+  ): Promise<FocusRecordDto> {
+    return this.focusRecordRepository.createFocusRecord(focusRecordDto);
+  }
+
+  // 포커스 레코드 업데이트
+  async updateFocusRecord(
+    id: string,
+    focusRecordDto: FocusRecordDto,
+  ): Promise<FocusRecordDto> {
+    return this.focusRecordRepository.updateFocusRecord(id, focusRecordDto);
+  }
+
+  // 포커스 레코드 삭제
+  deletedFocusRecord(id: string) {
+    this.focusRecordRepository.deleteFocusRecord(id);
+  }
+}
