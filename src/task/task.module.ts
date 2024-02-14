3728c7ba1cc419c36b911d88c977cf4a6b8f46da
hyeg0121 <w2218@e-mirim.hs.kr>
2024-02-14 22:49:16 +0900

    feat: task module

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TaskController } from './task.controller';
 import { TaskService } from './task.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Task, TaskSchema } from './task.schema';
+import { TaskRepository } from './task.repository';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Task.name, schema: TaskSchema }]),
+  ],
   controllers: [TaskController],
-  providers: [TaskService]
+  providers: [TaskRepository, TaskService],
 })
 export class TaskModule {}
