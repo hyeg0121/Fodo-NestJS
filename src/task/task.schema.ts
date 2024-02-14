27fc01f75febcfe21e5bcea8acaae0e5a3788153
hyeg0121 <w2218@e-mirim.hs.kr>
2024-02-14 22:31:42 +0900

    feat: task schema

---
+++ src/task/task.schema.ts
@@ -0,0 +1,27 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Schema as MongooseSchema } from 'mongoose';
+
+export type TaskDocument = Task & Document;
+
+@Schema()
+export class Task {
+  @Prop()
+  id: string;
+
+  @Prop({ type: MongooseSchema.Types.ObjectId, ref: 'Goal' })
+  goalId: string;
+
+  @Prop({ default: false })
+  isCompleted: boolean;
+
+  @Prop({ default: Date.now })
+  createdAt: Date;
+
+  @Prop({ default: Date.now })
+  updatedAt: Date;
+
+  @Prop({ default: false })
+  isDeleted: boolean;
+}
+
+export const TaskSchema = SchemaFactory.createForClass(Task);
