fe063e4a55ba27beea822105c3f142147c89e04d
hyeg0121 <w2218@e-mirim.hs.kr>
2024-02-14 20:06:07 +0900

    feat: goal service

--- src/goal/goal.service.ts
+++ src/goal/goal.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { GoalRepository } from './goal.repository';
+import { GoalDto } from './goal.model';
 
 @Injectable()
-export class GoalService {}
+export class GoalService {
+  constructor(private goalRepository: GoalRepository) {}
+
+  // 모든 목표 조회
+  async getAllGoals() {
+    return await this.goalRepository.getAllGoals();
+  }
+
+  // id로 목표 조회
+  async getGoalById(id: string): Promise<GoalDto> {
+    return await this.goalRepository.getGoalById(id);
+  }
+
+  // 목표 만들기
+  async createGoal(goalDto: GoalDto): Promise<GoalDto> {
+    return await this.goalRepository.createGoal(goalDto);
+  }
+
+  // 목표 업데이트
+  async updateGoal(id: string, goalDto: GoalDto): Promise<GoalDto> {
+    return this.goalRepository.updateGoal(id, goalDto);
+  }
+
+  // 목표 삭제
+  async deleteGoal(id: string) {
+    await this.goalRepository.deleteGoal(id);
+  }
+}
