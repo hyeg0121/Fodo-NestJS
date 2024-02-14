1c8cdddd0e5ea9d38aad314bc73fd2e7f06fd0ea
hyeg0121 <w2218@e-mirim.hs.kr>
2024-02-14 20:14:51 +0900

    feat: goal module

--- src/goal/goal.module.ts
+++ src/goal/goal.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { GoalController } from './goal.controller';
 import { GoalService } from './goal.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Goal, GoalSchema } from './goal.schema';
+import { GoalRepository } from "./goal.repository";
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Goal.name, schema: GoalSchema }]),
+  ],
   controllers: [GoalController],
-  providers: [GoalService]
+  providers: [GoalRepository, GoalService],
 })
 export class GoalModule {}
