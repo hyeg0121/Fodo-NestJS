5b0e7ec2b9bedb12223831094c26bd00d3c5fc40
hyeg0121 <w2218@e-mirim.hs.kr>
2024-02-15 23:42:07 +0900

    feat: CreateUserDto

--- src/user/user.model.ts
+++ src/user/user.model.ts
@@ -1,3 +1,5 @@
+import { IsEmail, IsString, IsStrongPassword } from "class-validator";
+
 export interface UserDto {
   id: string;
   name: string;
@@ -7,4 +9,16 @@ export interface UserDto {
   bio: string;
   created_at: Date;
   updated_at?: Date;
+}
+
+export class CreateUserDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  name: string;
+
+  // 8자 이상의 암호이며, 대문자, 소문자, 숫자, 특수 문자 중 하나 이상을 포함
+  @IsStrongPassword()
+  password: string;
 }
