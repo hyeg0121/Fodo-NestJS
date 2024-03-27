653c6c9c9d1c050b1caa2df18663e44d13e07877
hyeg0121 <w2218@e-mirim.hs.kr>
2024-03-27 22:20:07 +0900

    feat: 환경 변수 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { FocusModule } from './focus/focus.module';
 import { FocusRecordModule } from './focus-record/focus-record.module';
+import { FocusModule } from './focus/focus.module';
 import { GoalModule } from './goal/goal.module';
 import { TaskModule } from './task/task.module';
-import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/fodo'),
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
     UserModule,
     FocusModule,
     FocusRecordModule,
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
-
-  await app.listen(3000);
+  const PORT = process.env.PORT;
+  await app.listen(PORT);
 }
 bootstrap();
